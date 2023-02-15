9dc29b6c3866a53212c778f59fc4a73a037b4c85
gabrielrodrig0 <gabrielrodrigonaga@gmail.com>
2023-02-15 20:47:18 -0300

    Adição dos Seeders

--- backend/src/main/java/com/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dscatalog/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.dscatalog.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dscatalog.entities.Category;
 import com.dscatalog.repositories.CategoryRepository;
@@ -13,6 +14,8 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Transactional(readOnly = true) //Mantém a integridade da transação
 	public List<Category> findAll()
 	{
 		return categoryRepository.findAll();
